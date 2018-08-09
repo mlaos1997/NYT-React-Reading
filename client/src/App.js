64bb2b93ce6e7900af30222d720e4603c415348b
Marcelo Laos <mlaos1997@gmail.com>
2018-08-09 18:32:33 -0400

    updated routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => (
-  <div>
+  <Router>
+    <div>
     <Nav />
     <Books />
-  </div>
+    </div>
+  </Router>
 );
 
 export default App;
